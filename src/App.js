85401cef2f46039cda5bd50dad586f45a82780d9
SS <103727496+18bd1a0452@users.noreply.github.com>
2023-11-17 21:04:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [data, setdata] = useState([])
+  useEffect(() => {
+    setData();
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Today's {data?.location?.name}'s Weather Forecast <br></br>
+        Weather : {data?.current?.condition?.text}<br></br>
+        Humidity : {data?.current?.condition?.humidity}<br></br>
+        Pressure In : {data?.current?.pressure_in}<br></br>
+        Precipitation In : {data?.current?.precip_in}<br></br>
+        Temp (in C) : {data?.current?.temp_c}<br></br>
+        Wind Degree : {data?.current?.wind_degree}<br></br>
+        Wind Direction : {data?.current?.wind_dir}<br></br>
+        Wind Speed : {data?.current?.wind_kph}
       </header>
     </div>
   );
+
+  async function setData() {
+    await axios.get('https://api.weatherapi.com/v1/current.json?q=London&key=c6115257575243eab76165323231411')
+    .then(response => setdata(response.data))
+    .catch(error => console.log("Error", error))
+  }
 }
 
 export default App;
